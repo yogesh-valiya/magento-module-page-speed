cc1528a5467957daf7fbd5fc2ca9169b356506fb
Yogesh Valiya <yogesh.valiya@encora.com>
2022-12-08 16:45:13 +0530

    Added DNS preconnect processor

--- src/PageSpeed/Helper/OutputProcessorHelper.php
+++ src/PageSpeed/Helper/OutputProcessorHelper.php
@@ -3,12 +3,39 @@
 namespace YValiya\PageSpeed\Helper;
 
 use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Framework\Exception\LocalizedException;
 
 class OutputProcessorHelper extends AbstractHelper
 {
     public function appendToHead(string $content, string &$html): string
     {
         $html = str_replace("</head>", $content . "</head>", $html);
+        return $html;
+    }
+
+    /**
+     * @throws LocalizedException
+     */
+    public function prependToHead(string $content, string &$html): string
+    {
+        if (preg_match('/<head[^e][^<>]*>/', $html, $matches)) {
+            $headText = $matches[0] ?? false;
+            if ($headText) {
+                $html = str_replace($headText, $headText . $content, $html);
+            }
+        } else if (preg_match('/<meta[^<>]*charset[^<>]*>/', $html, $matches)) {
+            $charsetText = $matches[0] ?? false;
+            if ($charsetText) {
+                $html = str_replace($charsetText, $content . $charsetText, $html);
+            }
+        } else if (preg_match('/<meta[^<>]*name="title"[^<>]*>/', $html, $matches)) {
+            $titleText = $matches[0] ?? false;
+            if ($titleText) {
+                $html = str_replace($titleText, $content . $titleText, $html);
+            }
+        } else {
+            throw new LocalizedException(__("Failed to prepend content to head"));
+        }
         return $html;
     }
 }
--- src/PageSpeed/Model/Config.php
+++ src/PageSpeed/Model/Config.php
@@ -2,7 +2,7 @@
 
 namespace YValiya\PageSpeed\Model;
 
-class Config extends \Encora\ApiBase\Model\Config
+class Config extends \YValiya\Core\Model\Config
 {
     const XML_PATH_ENABLED = 'yvaliya_page_speed/general/enabled';
 }
